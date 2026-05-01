20dcdb3df2ebd37ccfed8bd8243aad8bf8b7f9ac
rahildave5 <rahildave05@gmail.com>
2026-05-02 00:30:29 +0530

    Home Page

--- src/landing_page/Home/Hero.jsx
+++ src/landing_page/Home/Hero.jsx
@@ -3,13 +3,13 @@ import homeHeroImage from '../../../../images/homeHero.png'
 
 function Hero() {
     return (
-        <section className='hero'>
+        <section className='hero' style={{ paddingBottom: '70px' }}>
             <div className="container p-5">
                 <div className="row text-center">
-                    <img className='mb-5' src={homeHeroImage} alt='Hero Image' />
+                    <img className='mb-3' style={{ marginLeft: '135px' }} src={homeHeroImage} alt='Hero Image' />
                     <h1 className='mt-3'>Invest in everything</h1>
                     <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                    <button className='mt-4 btn btn-primary fs-5' style={{ padding: "10px 30px", width: "21%", margin: "0 auto" }}>Sign up for free</button>
+                    <button className='mt-5 btn btn-primary fs-5' style={{ padding: "10px 30px", width: "21%", margin: "0 auto" }}>Sign up for free</button>
                 </div>
             </div>
         </section>
