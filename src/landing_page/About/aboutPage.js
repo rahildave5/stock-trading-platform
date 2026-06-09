cea1302bf2fb1c9a08007b3d2133b1b8d4475817
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-06-09 22:24:19 +0530

    Add About component with Hero, History, and Team

---
+++ src/landing_page/About/aboutPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./Hero.jsx";
+import History from "./History.jsx";
+import Team from "./Team.jsx";
+
+function About() {
+  return (
+    <>
+      <Hero />
+      <History />
+      <Team />
+    </>
+  );
+}
+
+export default About;
