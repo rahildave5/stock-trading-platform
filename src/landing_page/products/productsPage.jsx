07296b03fcaa74eaf387faa8270455fc9e62ec97
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-06-15 23:53:58 +0530

    Add Products component with multiple imports

--- src/landing_page/products/productsPage.jsx
+++ src/landing_page/products/productsPage.jsx
@@ -1,1 +1,27 @@
+import Hero from "./Hero.jsx";
+import Kite from "./kite.jsx";
+import Console from "../products/console.jsx";
+import Coin from "../products/coin.jsx";
+import Connect from "../products/connect.jsx";
+import Varsity from "../products/varsity.jsx";
+import Blog from "../products/blog.jsx";
+import Partners from "../products/partners.jsx";
+import Signup from "../products/signup.jsx";
 
+function Products() {
+  return (
+    <>
+      <Hero />
+      <Kite />
+      <Console />
+      <Coin />
+      <Connect />
+      <Varsity />
+      <Blog />
+      <Partners />
+      <Signup />
+    </>
+  );
+}
+
+export default Products;
