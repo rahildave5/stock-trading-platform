bef173c894cda8e0867ebe6ecce2cc9257f23788
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-06-18 23:59:52 +0530

    Update Education.jsx

--- src/landing_page/Home/Education.jsx
+++ src/landing_page/Home/Education.jsx
@@ -1,1 +1,49 @@
+import React from "react";
+import education from "../../../../images/education.svg";
 
+function Education() {
+  return (
+    <section className="education" style={{ paddingBottom: "120px" }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <img src={education} alt="Education" style={{ width: "74%" }} />
+          </div>
+
+          <div className="col-6">
+            <div className="edu-head">
+              <h2 className="mb-4">Free and open market education</h2>
+              <div className="edu-text">
+                <p className="mb-3" style={{ marginTop: "16px" }}>
+                  Varsity, the largest online stock market education book in the
+                  world covering everything from the basics to advanced trading.
+                </p>
+                <a className="fs-6" href="/">
+                  Varsity{" "}
+                  <i
+                    className="fa-solid fa-arrow-right"
+                    style={{ fontSize: "14px" }}
+                  ></i>
+                </a>
+
+                <p className="mb-3" style={{ marginTop: "30px" }}>
+                  TradingQ&A, the most active trading and investment community
+                  in India for all your market related queries.
+                </p>
+                <a className="fs-6" href="/">
+                  TradingQ&A{" "}
+                  <i
+                    className="fa-solid fa-arrow-right"
+                    style={{ fontSize: "14px" }}
+                  ></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Education;
