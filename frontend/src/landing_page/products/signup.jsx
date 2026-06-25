b47f7a3e44e3b7ad783a91fa4d597ad5185c264a
Rahil Dave <153646627+rahildave5@users.noreply.github.com>
2026-06-25 23:57:56 +0530

    Update signup.jsx

--- frontend/src/landing_page/products/signup.jsx
+++ frontend/src/landing_page/products/signup.jsx
@@ -1,1 +1,10 @@
+function Signup() {
+  return (
+    <div className="container py-4">
+      <h3>Signup</h3>
+      <p>Create your account to get started.</p>
+    </div>
+  );
+}
 
+export default Signup;
